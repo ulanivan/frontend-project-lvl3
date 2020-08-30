d1aceb0964d00351eb28d6de2b9ae4f185fa1252
ulanivan <ulanivan@yandex.ru>
2020-08-30 22:04:50 +0500

    rss data parsing

---
+++ src/parser.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+
+export default (data) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(data, 'application/xml');
+  const feed = doc.querySelector('channel');
+  const feedTitle = feed.querySelector('title').textContent;
+  const items = feed.querySelectorAll('item');
+  const id = _.uniqueId();
+  const feedItems = [...items].map((item) => {
+    const title = item.querySelector('title').textContent;
+    const link = item.querySelector('link').textContent;
+    const publicDate = new Date(item.querySelector('pubDate').textContent);
+    return { title, link, publicDate };
+  });
+  return { feedTitle, feedItems, id };
+};
